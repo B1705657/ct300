8a8939a6b3ccf19b20b345e66daa9aaf01254c25
B1705657 <hanhb1705657@student.ctu.edu.vn>
2023-10-18 21:13:31 +0700

    Baitap4

---
+++ for.cpp
@@ -0,0 +1,10 @@
+#include <stdio.h>
+#include <conio.h>
+
+int main()
+{
+	int num;
+	printf("chuong trinh for.c có 1 vòng l?p hi?n th? các s? t? 1 d?n 10: ");
+	for(num=1; num<=10; num +=1)
+		printf("%d\n", num);
+}
